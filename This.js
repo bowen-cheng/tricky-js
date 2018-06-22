2a892b925572f46a4958cc21f09d71a1290af288
Bowen Cheng <blueicycky@gmail.com>
2018-06-22 16:40:47 -0400

    Elaborate how 'this' keyword is handled in arrow functions

--- This.js
+++ This.js
@@ -1,13 +1,28 @@
+console.log('***************** Common function **************************');
+
 var name = "The Window";
 var object = {
     name: "The Object",
     callMe: function () {
-        console.log('Outter function: ' + this); // object
-        var that = this;
+        var self = this;
+        console.log(`<Outter function> this.name: ${this.name}`); // object
         return function () {
-            console.log('Inner function: ' + this); // window
-            return that.name;
+            console.log(`<Inner function> this.name: ${this.name}`); // window
+            console.log(`<Inner function> self.name: ${self.name}`); // object
         };
     }
 };
 object.callMe()();
+
+console.log('***************** Arrow function **************************');
+
+var arrowFunc = {
+    name: "The Object",
+    callMe: function () {
+        console.log(`<Outter function> this.name ${this.name}`); // object
+        return () => {
+            console.log(`<Inner function> this.name ${this.name}`); // window
+        };
+    }
+};
+arrowFunc.callMe()();
